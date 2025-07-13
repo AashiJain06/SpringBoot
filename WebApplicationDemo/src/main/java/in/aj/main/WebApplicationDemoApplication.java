fc0833c10e886c4cab588eedd6fcbcf0051ecb77
Aashi Jain <aashijain064@gmail.com>
2025-07-13 16:01:13 +0530

    Add files via upload

---
+++ WebApplicationDemo/src/main/java/in/aj/main/WebApplicationDemoApplication.java
@@ -0,0 +1,13 @@
+package in.aj.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplicationDemoApplication.class, args);
+	}
+
+}
