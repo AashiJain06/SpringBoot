952ffbe156d6832ae4762330f259ea36aef6a2e2
Aashi Jain <aashijain064@gmail.com>
2025-07-22 22:20:44 +0530

    Add files via upload

---
+++ SpringBootWithJPA/src/main/java/in/aj/main/entity/Student.java
@@ -0,0 +1,49 @@
+package in.aj.main.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table
+public class Student {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private long id;
+	@Column
+	private String name;
+	@Column
+	private String city;
+	@Column
+	private String email;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
