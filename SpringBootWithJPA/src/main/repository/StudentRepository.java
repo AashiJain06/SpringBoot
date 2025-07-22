69de59752d17264169c6cf21047943451de47f70
Aashi Jain <aashijain064@gmail.com>
2025-07-22 22:21:51 +0530

    Add files via upload

---
+++ SpringBootWithJPA/src/main/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package in.aj.main.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import in.aj.main.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student,Long>
+{
+       
+}
