97186710f99fb85f365f9ce4e13f724d149c069f
Aashi Jain <aashijain064@gmail.com>
2026-01-12 16:18:28 +0530

    Add files via upload

---
+++ SpringSecurityBasic/src/test/java/in/aj/main/SpringSecurityBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package in.aj.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
